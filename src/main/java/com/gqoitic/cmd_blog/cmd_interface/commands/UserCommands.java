017592a4c57d6266672cee454b604be96759a6cc
gqoitic <gqoitic@gmail.com>
2019-08-17 18:15:59 +0500

    change name fix

--- src/main/java/com/gqoitic/cmd_blog/cmd_interface/commands/UserCommands.java
+++ src/main/java/com/gqoitic/cmd_blog/cmd_interface/commands/UserCommands.java
@@ -2,6 +2,7 @@ package com.gqoitic.cmd_blog.cmd_interface.commands;
 
 import com.gqoitic.cmd_blog.cmd_interface.Indentions;
 import com.gqoitic.cmd_blog.cmd_interface.security.Authorization;
+import com.gqoitic.cmd_blog.model.User;
 
 import java.util.Scanner;
 
@@ -9,14 +10,23 @@ public class UserCommands {
 
     static Scanner scanner = new Scanner(System.in);
 
-    public static void changeName(){
+    public static boolean changeName(){
         Indentions.indention();
 
         System.out.print("\nEnter new name: ");
         String newName = scanner.nextLine();
 
+        for(User user : User.listOfUsers){
+            if(user.getName().equals(newName)){
+                System.out.print("\n\n\t=User with the same name already exists=\n");
+                return false;
+            }
+        }
+
         Authorization.currentUser.setName(newName);
+
         Indentions.indention();
+        return true;
     }
 
     public static void changePassword(){
