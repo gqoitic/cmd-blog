7b1d6e0a5abd9261c4c1d94b418c5dba6a2503fa
gqoitic <gqoitic@gmail.com>
2019-08-26 12:40:56 +0500

    ignore spaces in commnads

--- src/main/java/com/gqoitic/cmd_blog/cmd_interface/Interf_controller.java
+++ src/main/java/com/gqoitic/cmd_blog/cmd_interface/Interf_controller.java
@@ -1,12 +1,15 @@
 package com.gqoitic.cmd_blog.cmd_interface;
 
-import com.gqoitic.cmd_blog.cmd_interface.commands.CommonCommands;
 import com.gqoitic.cmd_blog.cmd_interface.commands.PostCommandsList;
 import com.gqoitic.cmd_blog.cmd_interface.commands.UserCommandsList;
 import com.gqoitic.cmd_blog.cmd_interface.security.Authorization;
 import com.gqoitic.cmd_blog.cmd_interface.security.Registration;
 
 import java.util.Scanner;
+
+import static com.gqoitic.cmd_blog.cmd_interface.commands.CommonCommands.*;
+import static com.gqoitic.cmd_blog.cmd_interface.commands.PostCommandsList.*;
+import static com.gqoitic.cmd_blog.cmd_interface.commands.UserCommandsList.*;
 
 public class Interf_controller {
 
@@ -22,45 +25,83 @@ public class Interf_controller {
             Indentions.pointer();
             String userInput = scanner.nextLine();
 
-            if(userInput.toUpperCase().equals(CommonCommands.LOGIN.getCommand())){
+            if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(LOGIN.getCommand())){
                 Authorization.login();
-            } else if(userInput.toUpperCase().equals(CommonCommands.EXIT.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(EXIT.getCommand())){
                 session = false;
-            } else if(userInput.toUpperCase().equals(CommonCommands.REGISTRATION.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+
+                    .equals(REGISTRATION.getCommand())){
                 Registration.registration();
-            } else if(userInput.toUpperCase().equals(UserCommandsList.SIGNOUT.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(SIGNOUT.getCommand())){
                 Authorization.signOut();
-            } else if(userInput.toUpperCase().equals(CommonCommands.CLEAR.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(CLEAR.getCommand())){
                 Indentions.clear();
-            } else if(userInput.toUpperCase().equals(CommonCommands.HELP.getCommand())){
-                CommonCommands.help();
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(HELP.getCommand())){
+                help();
+
             } else if(userInput.replaceAll("\\s+","").length() < 1) {}
 
-              else if(userInput.toUpperCase().equals(UserCommandsList.CHANGENAME.getCommand())){
+              else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(CHANGENAME.getCommand())){
                 UserCommandsList.changeName();
-            } else if(userInput.toUpperCase().equals(UserCommandsList.CHANGEPASSWORD.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(CHANGEPASSWORD.getCommand())){
                 UserCommandsList.changePassword();
-            } else if(userInput.toUpperCase().equals(UserCommandsList.NEWPOST.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(NEWPOST.getCommand())){
                   UserCommandsList.newPost();
-            } else if(userInput.toUpperCase().equals(PostCommandsList.SHOWALLPOSTS.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(SHOWALLPOSTS.getCommand())){
                 PostCommandsList.showAllPosts();
-            } else if(userInput.toUpperCase().equals(UserCommandsList.DELETEPOST.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(DELETEPOST.getCommand())){
                   UserCommandsList.deletePost();
-            } else if(userInput.toUpperCase().equals(UserCommandsList.DELETEUSER.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(DELETEUSER.getCommand())){
                   UserCommandsList.deleteUser();
-            } else if(userInput.toUpperCase().equals(UserCommandsList.CHANGEROLE.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(CHANGEROLE.getCommand())){
                   UserCommandsList.changeRole();
-            } else if(userInput.toUpperCase().equals(UserCommandsList.SHOWALLUSERS.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(SHOWALLUSERS.getCommand())){
                   UserCommandsList.showAllUsers();
-            } else if(userInput.toUpperCase().equals(PostCommandsList.SHOWPOSTSBYTITLE.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(SHOWPOSTSBYTITLE.getCommand())){
                   PostCommandsList.showPostsByTitle();
-            } else if(userInput.toUpperCase().equals(PostCommandsList.SHOWPOSTSBYAUTHOR.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(SHOWPOSTSBYAUTHOR.getCommand())){
                   PostCommandsList.showPostsByAuthor();
-            } else if(userInput.toUpperCase().equals(PostCommandsList.MYPOSTS.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(MYPOSTS.getCommand())){
                   PostCommandsList.myPosts();
-            } else if(userInput.toUpperCase().equals(PostCommandsList.CHANGETITLE.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(CHANGETITLE.getCommand())){
                   PostCommandsList.changeTitle();
-            } else if(userInput.toUpperCase().equals(PostCommandsList.CHANGETEXT.getCommand())){
+
+            } else if(userInput.toUpperCase().replaceAll("\\s+", "")
+                    .equals(CHANGETEXT.getCommand())){
                   PostCommandsList.changeText();
             }
              else {
